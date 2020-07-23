f2b57048e414c40aa555ae5625c5e6d9ec7267eb
Juan Cares <42586131+WoozieJuank@users.noreply.github.com>
2020-07-23 00:46:47 -0400

    Update camaras.js

--- api/routes/camaras.js
+++ api/routes/camaras.js
@@ -24,6 +24,11 @@ app.get('/camaras/:id', function (req, res) {
 app.get('/camaras', function (req, res) {
     res.send(camaras);
   });
+//retorna todas las camaras.
+app.get('/', function (req, res) {
+    res.send('<h1>Bienvenido a la calculadora</h1><br><br> <p>Realiza un cálculo simple utilizando la página https://apptic2.azurewebsites.net/{operación}/{número 1}/{número 2}</p><br><br><h2>El resultado de la multiplicación es: %s </h2>');
+  });
+
 // agrega una camara | en postman, escribir la siguiente sentencia: marca, referencia, precio e id
 app.put('/camaras',function (req,res ){
     
